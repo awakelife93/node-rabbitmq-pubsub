60f3e02a2fe0c550fdcafe2de0c50d5d76739c17
harry <awakelife93@gmail.com>
2022-06-20 19:47:16 +0900

    publishing response가 false일 시 todo 작성

--- src/producer/index.ts
+++ src/producer/index.ts
@@ -10,7 +10,8 @@ const publishTestMessages = (): void => {
   const sampleMessages = generateMessage();
 
   sampleMessages.forEach(async (message: string, index: number) => {
-    console.log(await Producer.sendMessage(config.QUEUE_NAME, message));
+    const response = await Producer.sendMessage(config.QUEUE_NAME, message);
+    // todo: response가 false일 시, 실패로 간주하고 로직 설계하기.
   });
 };
 
