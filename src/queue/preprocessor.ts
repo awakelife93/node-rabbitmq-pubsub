7e9160d105e483f112b84cc792ff5b36aa75a2ec
harry <awakelife93@gmail.com>
2022-06-23 00:05:14 +0900

    getQueueMessageCount throw error 제거

--- src/queue/preprocessor.ts
+++ src/queue/preprocessor.ts
@@ -1,5 +1,4 @@
 import RabbitMQChannel from "@/amqp/RabbitMQChannel";
-import { ErrorStatus } from "@/common/enum/error";
 import { getErrorItem } from "@/utils/error";
 
 export const getQueueMessageCount = async (
@@ -15,9 +14,7 @@ export const getQueueMessageCount = async (
     const _error = getErrorItem(error);
 
     // ! checkQueue는 해당 Queue가 없을 때 에러를 내면서 Channel을 소멸시키기 때문에... 우선 예외처리 감쌌음.
-    // * 각별히 조심해서 쓰자. 프로세스 꼬이면 안된다.
     console.log(`getQueueMessageCount Error: ${_error.message}`);
-    throw new Error(ErrorStatus.IS_EMPTY_QUEUE);
   }
 
   return messageCount;
