da81615966f8bb536334dbdf14aa8c7fa5492bff
hw.park <hw.park@fastfive.co.kr>
2022-06-23 18:56:47 +0900

    Broker Class로 변환

--- src/broker/index.ts
+++
@@ -1,9 +0,0 @@
-import consumer from "../consumer";
-
-const broker = async () => {
-  const { message, messageCount } = await consumer();
-  // todo: messageCount가 0이면 slow
-  console.log(`message ==========> ${message}`);
-};
-
-export default broker;
